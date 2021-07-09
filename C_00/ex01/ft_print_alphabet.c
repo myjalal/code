305dc2d89530949af3206e0fc96d3cf874fa323a
Jalal eddine Echekaoui <jechekao@c1r8p9.42quebec.com>
2021-07-08 21:37:00 -0400

    adding c_00

---
+++ C_00/ex01/ft_print_alphabet.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char alpha
+}
---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+void	reversalpha(void)
+{
+	char	alpha;
+
+	alpha = 'z';
+	while (alpha >= 'a')
+	{
+		write(1, &alpha, 1);
+		alpha--;
+	}
+}
